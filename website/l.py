4fb29c6dc06b8393dda675f24998bb03a1f02dca
sunil kumar <sunilkumar333221@gmail.com>
2022-08-20 18:01:54 +0530

    added files for deploment

---
+++ website/l.py
@@ -0,0 +1,8 @@
+class A:
+        def func(self):        
+            print("Hi")    
+        
+        def utrade(self):    
+            print "Hi, Utrade Solutions"       
+            
+A.func = A.utradea = A()a.func()
